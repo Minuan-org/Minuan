e472103931ed55ca8776a1d15e5b0c9c971738be
IamBobo <abdulsamad.arilewola@gmail.com>
2026-03-30 14:33:16 +0100

    Create utils.hpp

---
+++ include/utils.hpp
@@ -0,0 +1,14 @@
+#ifndef MINUAN_UTILS_HPP
+#define MINUAN_UTILS_HPP
+
+#include <string>
+
+namespace Utils {
+
+    void log(const std::string& msg);
+    void logWarning(const std::string& msg);
+    void logError(const std::string& msg);
+
+}
+
+#endif // MINUAN_UTILS_HPP
